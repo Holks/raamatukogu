01a870fdc74e11cbfc70b52a5c33437ac57128e6
Holger Kruusla <holgerkruusla@gmail.com>
2017-10-21 12:51:13 +0300

    uus projekt

---
+++ Raamatukogu/RaamauteAndmebaas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+ -- shortdesc
+
+ is a description
+
+It defines classes_and_methods
+
+@author:     user_name
+
+@copyright:  2017 organization_name. All rights reserved.
+
+@license:    license
+
+@contact:    user_email
+@deffield    updated: Updated
+'''

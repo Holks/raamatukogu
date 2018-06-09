08a7e0a1c04e8d09e7191ca81fd5628744520e9c
Holger Kruusla <holgerkruusla@gmail.com>
2018-06-10 01:28:36 +0300

    simple styling - bootstrap, database, views (HTML), adding books

--- Raamatukogu/app/models.py
+++ Raamatukogu/app/models.py
@@ -3,3 +3,24 @@
 from app import db
 from datetime import datetime
 from enum import Enum
+
+class Status(Enum):
+    archived = 0
+    present = 1
+    borrowed = 2
+    maintenance = 3
+    procesessing = 4
+    
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), index=True, nullable=False)
+    status = db.Column(db.Integer, default=1)
+    location_tag=db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), index=True, nullable=False)
+    publish_date = db.Column(db.String(5), nullable=False)
+    lend_date = db.Column(db.DateTime, index=True)
+    return_date = db.Column(db.DateTime, index=True )
+    isbn = db.Column(db.String(30), index=True, nullable=False)
+    description = db.Column(db.String(1000), default='')
+    def __repr__(self):
+        return {'id':self.id, 'title': self.title, 'status':self.status}
--- Raamatukogu/app/routes.py
+++ Raamatukogu/app/routes.py
@@ -4,9 +4,28 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from werkzeug.urls import url_parse
 import sys, os
+from app.models import Status, Book
+
+from flask import jsonify
+from flask import json
+from app import db
+
+import config
 
 @app.route('/', methods=['GET'])
 def index():
+    books = Book.query.all()
     
-    
-    return render_template('index.html') 
+    return render_template('index.html',books=books) 
+
+@app.route('/add', methods=['POST'])
+def add_book():
+    books = request.get_json()
+    if books:      
+        objects = []
+        for book in books:
+            book_to_add = Book(title=book['title'], status=Status.procesessing.value ,author=book['author'], isbn=book['isbn'], location_tag='check_in',publish_date=book['publish_date'][0:4])
+            objects.append(book_to_add)
+        db.session.bulk_save_objects(objects)
+        db.session.commit()        
+    return '',200 
